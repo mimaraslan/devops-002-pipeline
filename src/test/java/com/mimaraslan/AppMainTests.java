1b7d94197e1d05984d46cebc2e9557e20aa3e1a5
Mimar Aslan <mimaraslan@gmail.com>
2024-09-18 18:47:28 +0300

    DevOps Pipeline
    DevOps Pipeline

--- src/test/java/com/mimaraslan/AppMainTests.java
+++ src/test/java/com/mimaraslan/AppMainTests.java
@@ -5,10 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -26,7 +27,8 @@ class AppMainTests {
     void shouldReturnHelloWorld() throws Exception {
         this.mockMvc.perform(get("/info"))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().string("Hello World"));
+                .andExpect(content().string("DEVOPS INFO : " + LocalDateTime.now()));
     }
+
 
 }
