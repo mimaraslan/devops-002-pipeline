f4fdb442b1d376b8a3ecd3b90e1c04c3833dbfda
Mimar Aslan <mimaraslan@gmail.com>
2024-09-18 18:37:37 +0300

    DevOps Pipeline
    DevOps Pipeline

--- src/test/java/com/mimaraslan/AppMainTests.java
+++ src/test/java/com/mimaraslan/AppMainTests.java
@@ -1,13 +1,32 @@
 package com.mimaraslan;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AppMainTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnHelloWorld() throws Exception {
+        this.mockMvc.perform(get("/info"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string("Hello World"));
     }
 
 }
