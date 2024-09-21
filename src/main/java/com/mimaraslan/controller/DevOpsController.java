b44f0d4773697a48418562d787e21e075b039915
Mimar Aslan <mimaraslan@gmail.com>
2024-09-21 13:57:50 +0300

    Jekinsfile
    Jekinsfile

--- src/main/java/com/mimaraslan/controller/DevOpsController.java
+++ src/main/java/com/mimaraslan/controller/DevOpsController.java
@@ -16,12 +16,14 @@ public class DevOpsController {
     //  http://localhost:8081
     @GetMapping
     public String devopsHello() {
+        System.out.println("DevOps Hello : " + LocalDateTime.now());
         return "DevOps Hello : " + LocalDateTime.now();
     }
 
     //  http://localhost:8081/info
     @GetMapping("/info")
     public String info() {
+        System.out.println("DEVOPS INFO : " + LocalDateTime.now());
         return "DEVOPS INFO : " + LocalDateTime.now();
     }
 
